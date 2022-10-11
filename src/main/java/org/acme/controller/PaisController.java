1127f71f58c523a65f9d60961cfdadbb9ca89df4
avbravo <avbravo@avbravo>
2022-10-11 16:04:05 -0500

    1.0.5

--- src/main/java/org/acme/controller/PaisController.java
+++ src/main/java/org/acme/controller/PaisController.java
@@ -4,6 +4,7 @@
  */
 package org.acme.controller;
 
+import java.util.Collection;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -12,6 +13,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.acme.model.Pais;
 import org.acme.repository.PaisRepository;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 /**
  *
@@ -24,6 +30,11 @@ public class PaisController {
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+     @Operation(summary = "Get all conductor", description = "Returns all available items at the conductor")
+    @APIResponse(responseCode = "500", description = "Server unavailable")
+    @APIResponse(responseCode = "200", description = "The conductors")
+    @Tag(name = "BETA", description = "This API is currently in beta state")
+    @APIResponse(description = "The conductors", responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = Collection.class, readOnly = true, description = "the conductors", required = true, name = "conductors")))
     public List<Pais> findAll(){
         return paisRepository.findAll();
     }
